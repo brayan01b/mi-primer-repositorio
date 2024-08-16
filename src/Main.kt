a7129b2cbb346d233d61b0342c638ffaeef0b79a
brayan01b <brayanmelo01@gmail.com>
2024-08-16 16:21:17 -0500

    primer commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,32 @@
 import java.text.DecimalFormat
+import kotlin.random.Random
 
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-  println("mi primer commit")
-}
+ var lanzarMoneda=0
+var sello =2
+var cara  =1
+  var caeMoneda:Int
+  var contadorSello=0
+  var contadorCara=0
+  while (lanzarMoneda<=19){
+   val caeMoneda= Random.nextInt(1,3)
+    if(caeMoneda==sello){
+      contadorSello++
+      lanzarMoneda++
+
+
+    }else {
+      lanzarMoneda++
+      contadorCara++
+
+    }
+
+  }
+  println("cayo sello " + contadorSello)
+  println("cayo cara " + contadorCara)
+  }
+
 
