5c39c96bd331ff7cc5a0609c1c2fd746017391fb
brayan01b <brayanmelo01@gmail.com>
2024-08-16 15:07:55 -0500

    mi primer commit

---
+++ src/Main.kt
@@ -0,0 +1,9 @@
+import java.text.DecimalFormat
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+  println("mi primer commit")
+}
+
